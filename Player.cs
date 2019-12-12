c02ae21dccf6dabeea3f875e5e84c51f234de2fd
Opilane TTHK <opilane@tthk.ee>
2019-12-12 11:44:04 +0200

    game

---
+++ Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_player
+{
+    class Player
+    {
+        string name;
+        int healthPoints;
+
+        public Player(string _name)
+        {
+            Random rnd = new Random();
+            healthPoints = rnd.Next(100, 151);
+            name = _name;
+            
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public int HealthPoints
+        {
+            get { return healthPoints; }
+        }
+        public int MakeDamage()
+        {
+            Random rnd = new Random();
+            int damage= rnd.Next(10, 51);
+            return damage;
+        }
+        public void TakeDamage(int damage)
+        {
+         
+                healthPoints -= damage;
+            
+
+            
+            Console.WriteLine($"{healthPoints}");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Arnold Scwartzenegger");
+            Player player2 = new Player("Chuck Norris");
+
+            
+
+            for (int i =0; i < 3; i++)
+            {
+                player2.TakeDamage(player1.MakeDamage());
+                player1.TakeDamage(player2.MakeDamage());   
+
+            }
+            if (player1.HealthPoints > player2.HealthPoints)
+            {
+                Console.WriteLine($"Winner is {player1.Name} final health score is {player1.HealthPoints} ");
+            }
+            else if (player2.HealthPoints > player1.HealthPoints)
+            {
+                Console.WriteLine($"Winner is {player2.Name} final health score is {player2.HealthPoints} ");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
+               
+            Console.ReadLine();
+        }
+    }
+}
